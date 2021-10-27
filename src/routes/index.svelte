24abc0eca5dfd6d51453b428a37e6b7050e9ad51
teds31 <tstanowski31@gmail.com>
2021-10-27 10:11:00 -0700

    add animation gradient background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,63 @@
-<h1>My Website</h1>
-<p>welcome to my site</p>
+<div class="body">
+	<h1>My Website</h1>
+	<p>welcome to my site</p>
+</div>
+
+<style lang="postcss">
+	/* your styles go here */
+	.body {
+		height: 100vh;
+		background: linear-gradient(271deg, #3d2466, #b0344e, #3162c5);
+		background-size: 600% 600%;
+
+		-webkit-animation: AnimationName 11s ease infinite;
+		-moz-animation: AnimationName 11s ease infinite;
+		-o-animation: AnimationName 11s ease infinite;
+		animation: AnimationName 11s ease infinite;
+	}
+
+	@-webkit-keyframes AnimationName {
+		0% {
+			background-position: 0% 63%;
+		}
+		50% {
+			background-position: 100% 38%;
+		}
+		100% {
+			background-position: 0% 63%;
+		}
+	}
+	@-moz-keyframes AnimationName {
+		0% {
+			background-position: 0% 63%;
+		}
+		50% {
+			background-position: 100% 38%;
+		}
+		100% {
+			background-position: 0% 63%;
+		}
+	}
+	@-o-keyframes AnimationName {
+		0% {
+			background-position: 0% 63%;
+		}
+		50% {
+			background-position: 100% 38%;
+		}
+		100% {
+			background-position: 0% 63%;
+		}
+	}
+	@keyframes AnimationName {
+		0% {
+			background-position: 0% 63%;
+		}
+		50% {
+			background-position: 100% 38%;
+		}
+		100% {
+			background-position: 0% 63%;
+		}
+	}
+</style>
