715d4052dd1f9e518cb382ef0cc03decd3a48359
teds31 <tstanowski31@gmail.com>
2021-10-28 16:50:49 -0700

    add logo image to blog

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,4 +1,26 @@
-<h1>Teddy Code Bytes</h1>
+<script>
+	import logo from '../../../static/blog-logo.png';
+</script>
+
+<div class="max-w-screen-2xl">
+	<div class="flex justify-between p-4">
+		<a href="/">
+			<button class="bg-white rounded px-4 py-1 font-bold text-black shadow-lg">
+				Return To Main Page
+			</button>
+		</a>
+		<a href="/contact">
+			<button class="bg-white rounded px-4 py-1 font-bold text-black shadow-lg">
+				Contact Me
+			</button>
+		</a>
+	</div>
+</div>
+<div class="flex justify-center my-8">
+	<div class="mx-auto">
+		<img src={logo} alt="Teddy Code Bytes Blog" class="h-32" />
+	</div>
+</div>
 <div class="grid grid-cols-3 gap-12 px-8 my-8">
 	<article>
 		<div class="card bordered">
